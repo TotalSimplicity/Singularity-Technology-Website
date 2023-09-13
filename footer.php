74c9fa54eb266df4f3cee8ba9039f833ac4818bd
BuildTools <unconfigured@null.spigotmc.org>
2023-09-13 16:44:55 -0400

    Random stuff

--- footer.php
+++ footer.php
@@ -18,7 +18,5 @@
             <p class="copyright">Singularity Technology ©2023</p>
         </footer>
     </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
