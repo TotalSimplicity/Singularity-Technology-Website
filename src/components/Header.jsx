19ae208c98d74fe20ee736ea11b46985c2755fc8
TotalSimplicity <68793354+TotalSimplicity@users.noreply.github.com>
2024-03-18 19:42:31 -0400

    Create base project

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+
+    return (
+      
+        <div className="bg-slate-600 fixed w-screen h-16 flex flex-row items-center pl-8">
+            <h1 className="align-middle">Singularity Technology</h1>
+        </div>
+      
+    )
+  }
+export default Header
